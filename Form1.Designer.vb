a5da2828158d4ad00747e3d1d5360175dce0bf7d
Aleksandar@maggie-gw <sashkosnail@gmail.com>
2013-05-21 14:00:48 -0400

    Sample finished with regular threads

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,9 +22,145 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.StartStop = New System.Windows.Forms.Button()
+        Me.T1 = New System.Windows.Forms.TextBox()
+        Me.T2 = New System.Windows.Forms.TextBox()
+        Me.P = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.T1E = New System.Windows.Forms.TextBox()
+        Me.T2E = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'StartStop
+        '
+        Me.StartStop.Location = New System.Drawing.Point(107, 154)
+        Me.StartStop.Name = "StartStop"
+        Me.StartStop.Size = New System.Drawing.Size(75, 23)
+        Me.StartStop.TabIndex = 0
+        Me.StartStop.Text = "Start"
+        Me.StartStop.UseVisualStyleBackColor = True
+        '
+        'T1
+        '
+        Me.T1.Location = New System.Drawing.Point(27, 34)
+        Me.T1.Name = "T1"
+        Me.T1.ReadOnly = True
+        Me.T1.Size = New System.Drawing.Size(100, 20)
+        Me.T1.TabIndex = 1
+        '
+        'T2
+        '
+        Me.T2.Location = New System.Drawing.Point(164, 34)
+        Me.T2.Name = "T2"
+        Me.T2.ReadOnly = True
+        Me.T2.Size = New System.Drawing.Size(100, 20)
+        Me.T2.TabIndex = 2
+        '
+        'P
+        '
+        Me.P.Location = New System.Drawing.Point(93, 128)
+        Me.P.Name = "P"
+        Me.P.ReadOnly = True
+        Me.P.Size = New System.Drawing.Size(100, 20)
+        Me.P.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(27, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Thread1 Count"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(164, 14)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Thread2 Count"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(104, 112)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Process result"
+        '
+        'T1E
+        '
+        Me.T1E.Location = New System.Drawing.Point(27, 73)
+        Me.T1E.Name = "T1E"
+        Me.T1E.ReadOnly = True
+        Me.T1E.Size = New System.Drawing.Size(100, 20)
+        Me.T1E.TabIndex = 7
+        '
+        'T2E
+        '
+        Me.T2E.Location = New System.Drawing.Point(164, 73)
+        Me.T2E.Name = "T2E"
+        Me.T2E.ReadOnly = True
+        Me.T2E.Size = New System.Drawing.Size(100, 20)
+        Me.T2E.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(27, 57)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Thread1 Error"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(161, 57)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(72, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Thread2 Error"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(282, 184)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.T2E)
+        Me.Controls.Add(Me.T1E)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.P)
+        Me.Controls.Add(Me.T2)
+        Me.Controls.Add(Me.T1)
+        Me.Controls.Add(Me.StartStop)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents StartStop As System.Windows.Forms.Button
+    Friend WithEvents T1 As System.Windows.Forms.TextBox
+    Friend WithEvents T2 As System.Windows.Forms.TextBox
+    Friend WithEvents P As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents T1E As System.Windows.Forms.TextBox
+    Friend WithEvents T2E As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,135 @@
-﻿Public Class Form1
+﻿Imports System.Threading
 
+Public Class Form1
+
+    'The buffers below should be frame objects
+    'Array of arrays first level splits into old/new buffers, and then each element is for each camera frame buffer
+    Dim buffers = {{0, 0}, {0, 0}}
+
+    'Timeout Value
+    Const timeout As Integer = 2250
+
+    'Holding flags for camera communication timeouts
+    Dim camera_timout = {False, False}
+
+    'Holding errors for each camera thread
+    Dim camera_error = {"", ""}
+
+    Dim process_result As Integer = 0
+
+    Dim random_number As New System.Random
+
+    Dim keepRunning As Boolean = False
+
+    Dim mainThread As New Thread(AddressOf Me.MainThreadfunc)
+
+    'Error handling in the next two thread functions should be done through a memeber variable
+
+    Private Sub CameraThreadfunc(ByVal iCamNumber As Integer)  'The integer identifier here should be replaced with the camera handle
+        Thread.Sleep(CInt(1500.0 + random_number.NextDouble * 1000.0))    'Suspend thread for a random time from 1.5 to 2.5s
+        buffers(0, iCamNumber) += 1    'increment frame count in buffer used in display
+        If random_number.NextDouble > 0.99 Then camera_error(iCamNumber) = "ERROR"
+    End Sub
+
+    Private Sub ProcessThreadfunc(BufferID As Integer)
+        Thread.Sleep(CInt(100.0 + random_number.NextDouble * 150.0))    'Suspend thread for a random time from 0.1 to 0.25s
+        process_result = (buffers(1, 0) + buffers(1, 1)) '* random_number.NextDouble
+    End Sub
+
+    Private Sub MainThreadfunc()
+        Dim CameraThread1 As New Thread(AddressOf Me.CameraThreadfunc)
+        Dim CameraThread2 As New Thread(AddressOf Me.CameraThreadfunc)
+        Dim ProcessThread As New Thread(AddressOf Me.ProcessThreadfunc)
+
+        Dim updateUIDelegate = New MethodInvoker(AddressOf UpdateUI)
+
+        Static notFirstRun As Boolean = False
+        Try
+            While keepRunning
+                camera_timout = {False, False}
+                'New set of threads needs to be created for each run when no using a thread pool
+                CameraThread1 = New Thread(AddressOf Me.CameraThreadfunc)
+                CameraThread2 = New Thread(AddressOf Me.CameraThreadfunc)
+                ProcessThread = New Thread(AddressOf Me.ProcessThreadfunc)
+
+                'Start all threads in background
+                CameraThread1.IsBackground = True
+                CameraThread1.Start(0)
+                CameraThread2.IsBackground = True
+                CameraThread2.Start(1)
+
+                'Do not run PRocess on the first loop
+                If notFirstRun Then
+                    ProcessThread.IsBackground = True
+                    ProcessThread.Start()
+                Else
+                    notFirstRun = True
+                End If
+
+                'Wait for all started threads to finish before starting new iteration
+                If CameraThread1.IsAlive Then If CameraThread1.Join(timeout) Then camera_timout(0) = True
+                If CameraThread1.IsAlive Then If CameraThread2.Join(timeout) Then camera_timout(1) = True
+
+                If ProcessThread.IsAlive Then ProcessThread.Join()
+
+                'Save new data into the processign layer of the buffers
+                buffers(1, 0) = buffers(0, 0)
+                buffers(1, 1) = buffers(0, 1)
+
+                'Update the UI
+                Invoke(updateUIDelegate)
+
+                'Check if error occured and if so terminate thread
+                If camera_error(0) <> "" Or camera_error(1) <> "" Then Invoke(New MethodInvoker(AddressOf Failsafe))
+            End While
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Finally
+            'Cleanup ALWAYS RUNS
+            If CameraThread1.IsAlive Then CameraThread1.Join()
+            If CameraThread2.IsAlive Then CameraThread2.Join()
+            If ProcessThread.IsAlive Then ProcessThread.Join()
+        End Try
+    End Sub
+
+    Private Sub Failsafe()
+        StartStop_Click(Nothing, Nothing)
+    End Sub
+
+    Private Sub UpdateUI()
+        Try
+            T1.Text = buffers(0, 0).ToString
+            If camera_timout(0) Then T1.BackColor = Color.Red Else T1.BackColor = SystemColors.Control
+            T2.Text = buffers(0, 1).ToString
+            If camera_timout(1) Then T2.BackColor = Color.Red Else T2.BackColor = SystemColors.Control
+
+            P.Text = process_result.ToString
+
+            T1E.Text = camera_error(0)
+            T2E.Text = camera_error(1)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub StartStop_Click(sender As Object, e As EventArgs) Handles StartStop.Click
+        If keepRunning Then
+            StartStop.Text = "Start"
+            StartStop.BackColor = Color.Green
+            keepRunning = False
+        Else
+            StartStop.Text = "Stop"
+            StartStop.BackColor = Color.Red
+            keepRunning = True
+            'Make sure previous run has cleared
+            If mainThread.IsAlive Then mainThread.Join()
+            mainThread = New Thread(AddressOf Me.MainThreadfunc)
+            mainThread.IsBackground = True
+            mainThread.Start()
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        mainThread.IsBackground = True
+    End Sub
 End Class
